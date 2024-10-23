f408242f0e9303ed62395776877d580d3ef98646
Frank Fuhlbrück <ffuhlbrueck@bfs.de>
2024-10-23 16:30:12 +0200

    further testing and adopt coding style from non-test classes

--- src/test/java/de/bfs/dokpool/dokpoolTest.java
+++ src/test/java/de/bfs/dokpool/dokpoolTest.java
@@ -7,50 +7,80 @@ import org.junit.*;
 import de.bfs.dokpool.client.base.*;
 import de.bfs.dokpool.client.content.*;
 
-public class dokpoolTest
-{
-   private static final String ENCODING       = "UTF-8";
-   private static final String PACKAGE        = "de.bfs.dokpool";
-   private static final String PROTO          = System.getenv("DOKPOOL_PROTO");
-   private static final String HOST           = System.getenv("DOKPOOL_HOST");
-   private static final String PORT           = System.getenv("DOKPOOL_PORT");
-   private static final String PLONESITE      = System.getenv("DOKPOOL_PLONESITE");
-   private static final String USER           = System.getenv("DOKPOOL_USER");
-   private static final String PW             = System.getenv("DOKPOOL_PW");
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
-   /** Die main()-Methode ist nur fuer manuelle Testzwecke */
-   public static void main( String[] args ) throws Exception
-   {
+public class dokpoolTest {
+	private Log log = LogFactory.getLog(dokpoolTest.class);
 
-   }
+	private static final String ENCODING       = "UTF-8";
+	private static final String PACKAGE        = "de.bfs.dokpool";
+	private static final String PROTO          = System.getenv("DOKPOOL_PROTO");
+	private static final String HOST           = System.getenv("DOKPOOL_HOST");
+	private static final String PORT           = System.getenv("DOKPOOL_PORT");
+	private static final String PLONESITE      = System.getenv("DOKPOOL_PLONESITE");
+	private static final String USER           = System.getenv("DOKPOOL_USER");
+	private static final String PW             = System.getenv("DOKPOOL_PW");
+	private static final String DOKPOOL        = System.getenv("DOKPOOL_DOKPOOL");
+	private static final String GROUPFOLDER    = System.getenv("DOKPOOL_GROUPFOLDER");
 
-   @Test
-   public void someTestFunction() throws Exception
-   {
+	/** Die main()-Methode ist nur fuer manuelle Testzwecke */
+	public static void main( String[] args ) throws Exception {
 
+	}
 
-      System.out.println("URL: " + PROTO + "://" + HOST + ":" + PORT + "/" + PLONESITE + " User:" + USER + " Password:" + PW);
-//       //connect to Dokpool using API (wsapi4plone/wsapi4elan)
-      DocpoolBaseService docpoolBaseService = new DocpoolBaseService(PROTO + "://" + HOST + ":" + PORT + "/" + PLONESITE, USER, PW);
-      List<DocumentPool> myDocpools = docpoolBaseService.getDocumentPools();
-      System.out.println(myDocpools.size());
-      Assert.assertEquals( 5, 5 );
-   }
+	@Test
+	public void someTestFunction() throws Exception {
 
 
-   @Test
-   public void secondTestFunction() throws Exception
-   {
-      Assert.assertEquals( 5, 5 );
-   }
+		log.info("URL: " + PROTO + "://" + HOST + ":" + PORT + "/" + PLONESITE + " User:" + USER + " Password:" + PW);
+		DocpoolBaseService docpoolBaseService = new DocpoolBaseService(PROTO + "://" + HOST + ":" + PORT + "/" + PLONESITE, USER, PW);
+		List<DocumentPool> myDocpools = docpoolBaseService.getDocumentPools();
+		DocumentPool mainDocpool = docpoolBaseService.getPrimaryDocumentPool();
 
-   /**
-    * not used
-    */
-   static class HelperClass
-   {
-      public List<Object> elemente = new ArrayList<Object>();
+		log.info("Number of Dokppols: " + myDocpools.size());
+		log.info("Main Dokpool: " + mainDocpool.getFolderPath());
 
-   }
+		for (DocumentPool sDocpool : myDocpools) {
+			if (sDocpool.getFolderPath().matches("/" + PLONESITE + "/" + DOKPOOL)) {
+				mainDocpool = sDocpool;
+				log.info("Main Dokpool is now: " + mainDocpool.getFolderPath());
+				break;
+			}
+		}
+
+		Folder myGroupFolder = null;
+		try {
+			myGroupFolder = mainDocpool.getGroupFolders().get(0);
+		} catch (NullPointerException e) {
+			throw new NullPointerException("Could not find any valid GroupFolder for Dokpool " + mainDocpool.getFolderPath());
+		}
+
+		log.info("Group folder path (first from Dokpool): " + myGroupFolder.getFolderPath());
+
+		try {
+			myGroupFolder = mainDocpool.getFolder(/*mainDocpool.getFolderPath() + "/*/"content/Groups/" + GROUPFOLDER);
+			log.info("Group folder now set from from env: " +  myGroupFolder.getFolderPath());
+		} catch (NullPointerException e) {
+			log.warn("Could not find DOKPOOL_GROUPFOLDER: " + mainDocpool.getFolderPath() + "/content/Groups/" + GROUPFOLDER);
+			log.info("Group folder remains: " +  myGroupFolder.getFolderPath());
+		}
+
+		Assert.assertEquals(5,5);
+	}
+
+
+	@Test
+	public void secondTestFunction() throws Exception {
+		Assert.assertEquals(5,5);
+	}
+
+	/**
+	 * not used
+	 */
+	static class HelperClass {
+		public List<Object> elemente = new ArrayList<Object>();
+
+	}
 }
 
